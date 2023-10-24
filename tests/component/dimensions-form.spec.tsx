1903276b311d1279483865ba56fd53a85a851219
courtneypattison <courtneyp@ttison.ca>
2023-10-24 14:15:00 -0400

    Added DimensionsForm component

---
+++ tests/component/dimensions-form.spec.tsx
@@ -0,0 +1,42 @@
+import DimensionsForm from "@/_components/dimensions-form";
+import { expect, test } from "@playwright/experimental-ct-react";
+
+test("contain text 'Width'", async ({ mount }) => {
+  const component = await mount(<DimensionsForm handleSubmit={() => {}} />);
+  await expect(component).toContainText("Width");
+});
+
+test("contain text 'Height'", async ({ mount }) => {
+  const component = await mount(<DimensionsForm handleSubmit={() => {}} />);
+  await expect(component).toContainText("Height");
+});
+
+test("contain text 'Inches'", async ({ mount }) => {
+  const component = await mount(<DimensionsForm handleSubmit={() => {}} />);
+  await expect(component).toContainText("Inches");
+});
+
+test("contain text 'Centimetres'", async ({ mount }) => {
+  const component = await mount(<DimensionsForm handleSubmit={() => {}} />);
+  await expect(component).toContainText("Centimetres");
+});
+
+test("inches is checked", async ({ mount }) => {
+  const component = await mount(<DimensionsForm handleSubmit={() => {}} />);
+  const locator = component.getByRole("radio", { name: "inches" });
+  await expect(locator).toBeChecked();
+});
+
+test("enter width '7'", async ({ mount }) => {
+  const component = await mount(<DimensionsForm handleSubmit={() => {}} />);
+  const locator = component.getByRole("textbox", { name: "width" });
+  await locator.fill("7");
+  await expect(locator).toHaveValue("7");
+});
+
+test("cannot enter 'a'", async ({ mount }) => {
+  const component = await mount(<DimensionsForm handleSubmit={() => {}} />);
+  const locator = component.getByRole("textbox", { name: "width" });
+  await locator.fill("a");
+  await expect(locator).toHaveValue("");
+});
