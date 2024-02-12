963a2ac6bd279ad88c294a7969135ad8593fe720
courtneypattison <courtneyp@ttison.ca>
2024-02-12 15:18:57 -0500

    Stops scaling of PDF on smaller devices

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,8 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Pattern Projector",
   description: "Calibrate your projector for sewing patterns",
+  viewport:
+    "width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no",
 };
 
 export default function RootLayout({
