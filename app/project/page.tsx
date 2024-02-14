ed2697a37a57b1fd3a6c22066615d2df1ae31294
courtneypattison <courtneyp@ttison.ca>
2024-02-14 06:42:00 -0500

    Adds arrow back from project

--- app/project/page.tsx
+++ app/project/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Matrix from "ml-matrix";
+import Link from "next/link";
 import { ChangeEvent, MouseEvent, useEffect, useState } from "react";
 import { FullScreen, useFullScreenHandle } from "react-full-screen";
 
@@ -10,6 +11,7 @@ import Draggable from "@/_components/draggable";
 import FileInput from "@/_components/file-input";
 import FullScreenButton from "@/_components/full-screen-button";
 import PDFViewer from "@/_components/pdf-viewer";
+import ArrowBackIcon from "@/_icons/arrow-back-icon";
 import FlipHorizontalIcon from "@/_icons/flip-horizontal-icon";
 import FlipVerticalIcon from "@/_icons/flip-vertical-icon";
 import InvertColorIcon from "@/_icons/invert-color-icon";
@@ -175,6 +177,9 @@ export default function Page() {
     <main>
       <FullScreen handle={handle}>
         <div className="flex flex-wrap items-center gap-4 m-4">
+          <Link href="/">
+            <ArrowBackIcon />
+          </Link>
           <button
             className="z-10 text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
             onClick={() => setIsCalibrating(!isCalibrating)}
