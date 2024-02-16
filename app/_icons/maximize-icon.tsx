ccd63d38f2f97ba519ced7d0da1417253abf8005
courtneypattison <courtneyp@ttison.ca>
2024-02-16 13:48:51 -0500

    Changes stroke-width to strokeWidth

--- app/_icons/maximize-icon.tsx
+++ app/_icons/maximize-icon.tsx
@@ -34,7 +34,7 @@ export default function MaximizeIcon() {
         stroke="currentColor"
         stroke-linecap="round"
         stroke-linejoin="round"
-        stroke-width="2"
+        strokeWidth="2"
         d="M8 4H4m0 0v4m0-4 5 5m7-5h4m0 0v4m0-4-5 5M8 20H4m0 0v-4m0 4 5-5m7 5h4m0 0v-4m0 4-5-5"
       />
     </svg>
