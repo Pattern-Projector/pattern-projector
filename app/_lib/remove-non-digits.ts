015b8318e10ffb3ee4119b4088e186e5c5e27d1e
courtneypattison <courtneyp@ttison.ca>
2024-02-16 06:30:26 -0500

    Accepts floating point numbers for

--- app/_lib/remove-non-digits.ts
+++ app/_lib/remove-non-digits.ts
@@ -1,8 +1,8 @@
 /**
  * Globally removes non digits from a string
  * @param s - string to remove non digits from
- * @returns - a new string with only digits in the order they appear in s
+ * @returns - a new string with only digits or decimal points in the order they appear in s
  */
 export default function removeNonDigits(s: string) {
-  return s.replace(/\D/g, "");
+  return s.replace(/[^.\d]/g, "");
 }
