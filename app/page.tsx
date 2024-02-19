14d3c4a5173d87b53a8d171945d21fca131956f2
courtneypattison <courtneyp@ttison.ca>
2024-02-19 12:17:39 -0500

    Adds instructions to landing page

--- app/page.tsx
+++ app/page.tsx
@@ -14,102 +14,113 @@ export default function Home() {
       <nav className="flex m-4 items-center">
         <PatternProjectorIcon />
         <Link
-          className="ml-auto text-white bg-gray-800 border border-gray-600 focus:outline-none hover:bg-gray-700 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5"
+          href="https://github.com/Pattern-Projector/pattern-projector"
+          className="ml-auto"
+        >
+          <GithubIcon />
+        </Link>
+        <Link
+          className="ml-4 text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
           href="/calibrate"
         >
           Start Calibrating
         </Link>
       </nav>
-      <h1>Hello, Welcome to Pattern Projector!</h1>
-      <h2>How to use Pattern Projector</h2>
-      <ol>
-        <li>
-          1. Connect the projector to a computer or tablet, either directly with
-          an HDMI cable or with a casting device.
-        </li>
-        <li>
-          2. Focus and keystone projector. Usually this process involves moving
-          a dial on the projector until the picture looks clear and moving
-          another dial until the output looks like a rectangle instead of a
-          trapezoid.
-        </li>
-        <li>
-          3. Go to the <Link href="/calibrate">Calibration page</Link> and drag
-          the corners of the calibration rectangle to match the fathest corners
-          of a cutting mat or another flat rectangle of known size (e.g. a piece
-          of bristol board).
-        </li>
-        <li>
-          4. Enter the width and height—in inches—of the rectangular object that
-          the calibration rectangle is lined up with (e.g. width: 22.5 and
-          height 28.5 for the bristol board).
-        </li>
-        <li>
-          4. Click the Show Pattern button and choose a pattern file to project.
-        </li>
-        <li>5. Start cutting!</li>
-        <li>
-          6. Use pattern projector tools to help in the cutting process:{" "}
-          <ul>
-            <li>
-              <InvertColorIcon /> Invert PDF colors to make projection easier to
-              see
-            </li>
-            <li>
-              <FlipVerticalIcon /> Flip vertically
-            </li>
-            <li>
-              <FlipHorizontalIcon /> Flip horizontally
-            </li>
-            <li>
-              <Rotate90DegreesCWIcon /> Rotate 90° Clockwise
-            </li>
-            <li>
-              <MaximizeIcon /> Enter full screen mode (It is best to calibrate
-              in full screen mode if cutting in full screen mode)
-            </li>
-          </ul>
-        </li>
-      </ol>
-      <h2>About</h2>
-      <p>
-        Pattern Projector is an open source website that allows users to quickly
-        calibrate projectors for sewing patterns.
-      </p>
-      <p></p>
+      <article className="prose lg:prose-xl m-auto">
+        <h1 id="welcome-to-pattern-projector">Welcome to Pattern Projector!</h1>
+        <p>
+          Pattern projector is a free and open source web app that helps users
+          quickly calibrate projectors for sewing patterns. This project is
+          currently in beta, so expect large changes and new features to be
+          added as we iterate.
+        </p>
+        <h2 id="what-youll-need">What You’ll Need</h2>
+        <ul>
+          <li>Projector: at least 720p recommended</li>
+          <li>Cutting mat: ideally with grid lines at every inch</li>
+          <li>Tripod or wall/shelf/table mount for projector</li>
+          <li>Computer or tablet to connect to the projector</li>
+          <li>A PDF sewing pattern: single layer, single page PDF is best</li>
+        </ul>
+        <h2 id="setup">Setup</h2>
+        <p>
+          Place the projector above the cutting mat, pointing at the cutting
+          mat. Try to place the projector directly above the cutting mat and
+          pointing directly at it.
+        </p>
+        <p>
+          Connect your computer or tablet to the projector and either mirror or
+          extend the display.
+        </p>
+        <p>
+          Adjust the focus on the projector, until text is crisp in the centre
+          of the projection. If you cannot get a clear image, ensure the
+          distance between the projector and cutting mat is within the
+          functional range recommended by the manufacturer.
+        </p>
+        <p>
+          If your projector has a keystone, adjust it so that projection is
+          close to rectangular and focus near the edges improves.
+        </p>
+        <h2 id="calibration">Calibration</h2>
+        <p>Open “Calibration”.</p>
+        <p>Enter the width and height of your mat into the page.</p>
+        <p>Enter fullscreen mode by clicking (or tapping) “Fullscreen”</p>
+        <p>
+          Drag the corners of the grid to align with your mat. With your eyes on
+          the mat, adjust the corners on the tablet or computer. Adjust the
+          placement of the corners until the projected grid matches your mat’s
+          grid.
+        </p>
+        <p>
+          When the projected grid is aligned with your mat, click (or tap)
+          “Project Pattern”
+        </p>
+        <h2 id="projecting-a-pattern">Projecting a Pattern</h2>
+        <p>Click (or tap) “Open File” to load the PDF document.</p>
+        <p>Cut along the projected design.</p>
+        <p>In projection mode there are several tools provided:</p>
+        <ul>
+          <li>
+            <p>Drag to move: Move the PDF by dragging it around the screen.</p>
+          </li>
+          <li>
+            <p>
+              Invert Colours: When projecting, it’s usually easier to see white
+              lines on black. Click “invert” to invert the colours.
+            </p>
+          </li>
+          <li>
+            <p>
+              Flip Vertical/Horizontal: helpful to unwrap fold lines in patterns
+            </p>
+          </li>
+          <li>
+            <p>Rotate: to change the orientation of the pattern</p>
+          </li>
+          <li>
+            <p>
+              Fullscreen: It’s generally easier to use the software in
+              fullscreen mode, tap or click “Fullscreen”
+            </p>
+          </li>
+        </ul>
+        <h2 id="faq">FAQ</h2>
+        <p>
+          Layer Support: planned for upcoming release. Annotation Support:
+          planned for upcoming release. Chromecast/Miracast: While it’s possible
+          to cast this webpage, the lag in the connection can be frustrating,
+          especially when calibrating. Phones: While possible to visit and use
+          the webpage on a Phone, the limited screen size makes it difficult to
+          use.
+        </p>
+        <p>Feedback and feature requests are welcome.</p>
+      </article>
       <footer className="flex flex-col items-center bg-neutral-900 text-center text-white gap-4 p-4">
-        <Link href="https://github.com/Pattern-Projector/pattern-projector">
-          <GithubIcon />
-        </Link>
-
-        <div>
-          <form action="">
-            <div className="grid-cols-1 grid items-center justify-center gap-4 md:grid-cols-3">
-              <p className="">
-                <strong>Sign up for the newsletter</strong>
-              </p>
-
-              <input
-                type="text"
-                className="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] text-neutral-200 outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
-                id="email"
-                placeholder="Email address"
-              />
-              <label htmlFor="email" className="font-bold text-white">
-                Email address
-              </label>
-              <button
-                type="submit"
-                className="text-white bg-gray-800 border border-gray-600 focus:outline-none hover:bg-gray-700 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5"
-              >
-                Subscribe
-              </button>
-            </div>
-          </form>
-        </div>
-
         <h2 className="font-bold uppercase">Contact</h2>
-        <p>courtney@patternprojector.com</p>
+        <a href="mailto:courtney@patternprojector.com">
+          courtney@patternprojector.com
+        </a>
 
         <div className="w-full p-4 text-center">© 2024 Courtney Pattison</div>
       </footer>
