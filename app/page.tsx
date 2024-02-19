7c0417a68dc91c69ed5c759c670bf0b8c8afff1a
courtneypattison <courtneyp@ttison.ca>
2024-02-19 15:43:50 -0500

    Adds icons to landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Link from "next/link";
 
+import FullscreenIcon from "@/_icons/fullscreen-icon";
+
 import FlipHorizontalIcon from "./_icons/flip-horizontal-icon";
 import FlipVerticalIcon from "./_icons/flip-vertical-icon";
-import MaximizeIcon from "./_icons/fullscreen-icon";
 import GithubIcon from "./_icons/github-icon";
 import InvertColorIcon from "./_icons/invert-color-icon";
 import PatternProjectorIcon from "./_icons/pattern-projector-icon";
+import PdfIcon from "./_icons/pdf-icon";
 import Rotate90DegreesCWIcon from "./_icons/rotate-90-degrees-cw-icon";
 
 export default function Home() {
@@ -13,6 +15,7 @@ export default function Home() {
     <main className="m-4">
       <nav className="flex items-center">
         <PatternProjectorIcon />
+        <span className="font-bold">beta</span>
         <Link
           href="https://github.com/Pattern-Projector/pattern-projector"
           className="ml-auto"
@@ -20,7 +23,7 @@ export default function Home() {
           <GithubIcon />
         </Link>
         <Link
-          className="ml-4 text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+          className="ml-4 text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
           href="/calibrate"
         >
           Start Calibrating
@@ -40,7 +43,11 @@ export default function Home() {
           <li>Cutting mat: ideally with grid lines at every inch</li>
           <li>Tripod or wall/shelf/table mount for projector</li>
           <li>Computer or tablet to connect to the projector</li>
-          <li>A PDF sewing pattern: single layer, single page PDF is best</li>
+          <li>
+            A PDF sewing pattern: consider using{" "}
+            <a href="https://www.pdfstitcher.org/">PDFStitcher</a> if your
+            pattern has layers or multiple pages
+          </li>
         </ul>
         <h2 id="setup">Setup</h2>
         <p>
@@ -63,66 +70,103 @@ export default function Home() {
           close to rectangular and focus near the edges improves.
         </p>
         <h2 id="calibration">Calibration</h2>
-        <p>Open “Calibration”.</p>
-        <p>Enter the width and height of your mat into the page.</p>
-        <p>Enter fullscreen mode by clicking (or tapping) “Fullscreen”</p>
+        <p>Click (or tap) “Start Calibrating.”</p>
+        <p>Input the width and height of your mat.</p>
+        <p className="flex mr-4 gap-4">
+          Enter fullscreen mode by clicking (or tapping) <FullscreenIcon />
+        </p>
         <p>
           Drag the corners of the grid to align with your mat. With your eyes on
-          the mat, adjust the corners on the tablet or computer. Adjust the
-          placement of the corners until the projected grid matches your mat’s
-          grid.
+          the mat, adjust the corners on your tablet or computer. Adjust the
+          placement of the corners until the projected grid matches your
+          mat&apos;s grid.
         </p>
         <p>
           When the projected grid is aligned with your mat, click (or tap)
-          “Project Pattern”
+          “Project.”
         </p>
         <h2 id="projecting-a-pattern">Projecting a Pattern</h2>
-        <p>Click (or tap) “Open File” to load the PDF document.</p>
+        <p className="flex mr-4 gap-4">
+          Click (or tap)
+          <PdfIcon /> to load the PDF document.
+        </p>
         <p>Cut along the projected design.</p>
         <p>In projection mode there are several tools provided:</p>
         <ul>
           <li>
-            <p>Drag to move: Move the PDF by dragging it around the screen.</p>
-          </li>
-          <li>
             <p>
-              Invert Colours: When projecting, it’s usually easier to see white
-              lines on black. Click “invert” to invert the colours.
+              Drag to move: move the PDF by clicking and dragging it around the
+              screen.
             </p>
           </li>
           <li>
+            <div className="flex gap-4">
+              <InvertColorIcon />
+            </div>
             <p>
-              Flip Vertical/Horizontal: helpful to unwrap fold lines in patterns
+              Invert Colors: when projecting, it&apos;s usually easier to see
+              white lines on black.
             </p>
           </li>
           <li>
-            <p>Rotate: to change the orientation of the pattern</p>
+            <div className="flex gap-4">
+              <FlipVerticalIcon />
+              <FlipHorizontalIcon />
+            </div>
+
+            <p>
+              Flip Vertical/Horizontal: helpful to unwrap fold lines in
+              patterns.
+            </p>
           </li>
           <li>
+            <div className="flex gap-4">
+              <Rotate90DegreesCWIcon />
+            </div>
+            <p>Rotate: to change the orientation of the pattern.</p>
+          </li>
+          <li>
+            <div className="flex gap-4">
+              <FullscreenIcon />
+            </div>
             <p>
-              Fullscreen: It’s generally easier to use the software in
-              fullscreen mode, tap or click “Fullscreen”
+              Fullscreen: it&apos;s generally easier to use the software in
+              fullscreen mode.
             </p>
           </li>
         </ul>
         <h2 id="faq">FAQ</h2>
-        <p>
-          Layer Support: planned for upcoming release. Annotation Support:
-          planned for upcoming release. Chromecast/Miracast: While it’s possible
-          to cast this webpage, the lag in the connection can be frustrating,
-          especially when calibrating. Phones: While possible to visit and use
-          the webpage on a Phone, the limited screen size makes it difficult to
-          use.
-        </p>
-        <p>Feedback and feature requests are welcome.</p>
+        <ul>
+          <li>Layer Support: planned for upcoming release.</li>
+          <li>Annotation Support: planned for upcoming release.</li>
+          <li>
+            Chromecast/Miracast: While it&apos;s possible to cast this webpage,
+            the lag in the connection can be frustrating, especially when
+            calibrating.
+          </li>
+          <li>
+            Phones: While possible to visit and use the webpage on a Phone, the
+            limited screen size makes it difficult to use.
+          </li>
+        </ul>
+        <p>Feedback and feature requests are welcome!</p>
       </article>
-      <footer className="flex flex-col items-center bg-neutral-900 text-center text-white gap-4 p-4">
-        <h2 className="font-bold uppercase">Contact</h2>
-        <a href="mailto:courtney@patternprojector.com">
-          courtney@patternprojector.com
-        </a>
+      <footer className="bg-white rounded-lg shadow m-4">
+        <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+            © 2024{" "}
+            <a href="https://courtneypattison.com/" className="hover:underline">
+              Courtney Pattison
+            </a>
+          </span>
 
-        <div className="w-full p-4 text-center">© 2024 Courtney Pattison</div>
+          <a
+            href="mailto:courtney@patternprojector.com"
+            className="hover:underline"
+          >
+            Contact
+          </a>
+        </div>
       </footer>
     </main>
   );
