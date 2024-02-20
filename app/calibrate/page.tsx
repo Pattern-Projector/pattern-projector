878be3c7b31c27ee16865cbac5d31f2bf9dedf2d
courtneypattison <courtneyp@ttison.ca>
2024-02-20 05:47:21 -0500

    Fixes show full screen icon on calibrate page

--- app/calibrate/page.tsx
+++ app/calibrate/page.tsx
@@ -358,9 +358,7 @@ export default function Page() {
               <DeleteIcon />
             </button>
             <FullScreenButton
-              className={`bg-white z-20 cursor-pointer from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-full p-2.5 ${visible(
-                !isCalibrating
-              )}`}
+              className={`bg-white z-20 cursor-pointer from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-full p-2.5`}
               handle={handle}
             />
           </div>
