6d7a7628f6f3e7e205dad60647c63e16f0ee6145
courtneypattison <courtneyp@ttison.ca>
2024-02-24 15:43:57 -0500

    Add support for danish translation

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import createMiddleware from 'next-intl/middleware';
  
 export default createMiddleware({
   // A list of all locales that are supported
-  locales: ['en', 'de'],
+  locales: ['en', 'de', 'da'],
  
   // Used when no locale matches
   defaultLocale: 'en'
@@ -10,5 +10,5 @@ export default createMiddleware({
  
 export const config = {
   // Match only internationalized pathnames
-  matcher: ['/', '/(de|en)/:path*']
+  matcher: ['/', '/(de|en|da)/:path*']
 };
--- navigation.ts
+++ navigation.ts
@@ -1,6 +1,6 @@
 import { createSharedPathnamesNavigation } from 'next-intl/navigation';
 
-export const locales = ['en', 'de'] as const;
+export const locales = ['en', 'de', 'da'] as const;
 export const localePrefix = 'always'; // Default
  
 export const {Link, redirect, usePathname, useRouter} =
