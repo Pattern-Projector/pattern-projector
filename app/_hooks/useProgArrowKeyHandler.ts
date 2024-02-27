62c2e7b952ebce490bf75e329e412245cb456d7b
Hannah L <vballprincess859@hotmail.com>
2024-02-27 17:34:50 -0500

    Branch switch to pull another branch

---
+++ app/_hooks/useProgArrowKeyHandler.ts
@@ -0,0 +1,10 @@
+import { useState } from "react";
+
+function useProgArrowKeyHandler() {
+  const [pixelGap, setPixelGap] = useState<number>(1);
+
+  function keydownHandler() {}
+
+  if (typeof window !== "undefined") {
+  }
+}
