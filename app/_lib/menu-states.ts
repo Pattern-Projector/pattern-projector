72e1126d068cabdbff76472f71ef431fed191518
Hannah L <vballprincess859@hotmail.com>
2024-03-23 11:01:32 -0400

    adds a changelog. Updates stitch menu to be horizontal. Changes some buttons and transitions to feel a little cleaner

---
+++ app/_lib/menu-states.ts
@@ -0,0 +1,41 @@
+import { Layer } from "@/_lib/layer";
+
+export interface MenuStates {
+  nav: boolean;
+  layers: boolean;
+  stitch: boolean;
+}
+
+export function getDefaultMenuStates(): MenuStates {
+  return {
+    nav: true,
+    layers: false,
+    stitch: false,
+  };
+}
+
+export function getMenuStatesFromPageCount(
+  menuStates: MenuStates,
+  pageCount: number,
+) {
+  let showStitch: boolean = menuStates.stitch;
+  if (pageCount === 1) {
+    showStitch = false;
+  } else if (pageCount > 1) {
+    showStitch = true;
+  }
+  return { ...menuStates, stitch: showStitch };
+}
+
+export function getMenuStatesFromLayers(
+  menuStates: MenuStates,
+  layers: Map<string, Layer>,
+) {
+  let showLayers;
+  if (layers.size > 0) {
+    showLayers = true;
+  } else {
+    showLayers = false;
+  }
+  return { ...menuStates, layers: showLayers };
+}
