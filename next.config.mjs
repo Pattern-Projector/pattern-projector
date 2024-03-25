ab120384c775e7a19c88ebb4df7526cdc5717b85
courtneypattison <courtneyp@ttison.ca>
2024-03-25 15:20:21 -0400

    Disable offline mode for development

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,12 @@
 import withSerwistInit from "@serwist/next";
 import createNextIntlPlugin from 'next-intl/plugin';
-      
+   
 const withSerwist = withSerwistInit({
     swSrc: "app/sw.ts",
     swDest: "public/sw.js",
     cacheOnFrontEndNav: true,
     reloadOnOnline: true,
+    disable: process.env.NODE_ENV === "development",
 });
  
 const withNextIntl = createNextIntlPlugin();
