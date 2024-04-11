9a3b729bd23c536aa27de86f7c00065dc77c6787
courtneypattison <courtneyp@ttison.ca>
2024-04-11 06:24:34 -0400

    Refactor layer menu component and add translations

--- app/_components/layer-menu.tsx
+++ app/_components/layer-menu.tsx
@@ -2,6 +2,8 @@ import { Layer } from "@/_lib/interfaces/layer";
 import { Dispatch, SetStateAction, useMemo } from "react";
 import { IconButton } from "@/_components/buttons/icon-button";
 import CloseIcon from "@/_icons/close-icon";
+import { useTranslations } from "next-intl";
+import LayersIcon from "@/_icons/layers-icon";
 
 export default function LayerMenu({
   className,
@@ -16,6 +18,8 @@ export default function LayerMenu({
   visible: boolean;
   setVisible: (visible: boolean) => void;
 }) {
+  const t = useTranslations("LayerMenu");
+
   function handleOnChange(key: string, layer: Layer) {
     layer.visible = !layer.visible;
     setLayers(new Map(layers.set(key, layer)));
@@ -36,53 +40,63 @@ export default function LayerMenu({
   }
 
   return (
-    <menu
-      style={{ height: "calc(100vh - 19rem)" }}
-      className={`${className} ${visible ? "left-0" : "-left-60"} w-48 text-sm font-medium text-gray-900 dark:text-white bg-white dark:bg-black border border-gray-200 dark:border-gray-700 border-top-0 absolute z-50 transition-all duration-700`}
+    <div
+      className={`${className} ${visible ? "left-0" : "-left-60"} w-48 h-[calc(100vh-9rem)] text-sm font-medium text-gray-900 dark:text-white bg-white dark:bg-black border border-gray-200 dark:border-gray-700 border-top-0 absolute z-50 transition-all duration-700`}
     >
-      <li
+      <h1
         key="global"
         className="w-full border-b border-gray-200 dark:border-gray-700 rounded-t-lg"
       >
-        <div className="flex items-center justify-between ps-3">
-          <div className="flex items-center">
-            <input
-              id="global"
-              type="checkbox"
-              className="w-4 h-4 accent-purple-600 bg-gray-100 border-gray-300 dark:border-gray-700 rounded focus:ring-blue-500 dark:focus:ring-blue-800"
-              checked={globalCheck}
-              style={{ width: 14 }}
-              onChange={(e) => handleGlobalChange(e.target.checked)}
-            />
-            <h6 className="ml-2">Layers</h6>
-          </div>
-          <IconButton onClick={() => setVisible(false)}>
+        <div className="flex items-center space-between ml-2">
+          <LayersIcon ariaLabel={t("title")} />
+          <h6 className="ml-2">{t("title")}</h6>
+          <IconButton onClick={() => setVisible(false)} className="ml-auto">
             <CloseIcon ariaLabel="close" />
           </IconButton>
         </div>
-      </li>
-      {[...layers.entries()].map((e) => (
-        <li
-          key={e[0]}
-          className="w-full border-b border-gray-200 dark:border-gray-700 rounded-t-lg"
-        >
-          <div className="flex items-center ps-3">
-            <input
-              id={e[0]}
-              type="checkbox"
-              className="w-4 h-4 accent-purple-600 bg-gray-100 border-gray-300 dark:border-gray-700 rounded focus:ring-blue-500 dark:focus:ring-blue-800"
-              checked={e[1].visible}
-              onChange={() => handleOnChange(e[0], e[1])}
-            ></input>
-            <label
-              htmlFor={e[0]}
-              className="w-full py-3 ms-2 text-sm font-medium text-gray-90"
-            >
-              {e[1].name}
-            </label>
-          </div>
-        </li>
-      ))}
-    </menu>
+      </h1>
+      <div className="w-full border-b border-gray-200 dark:border-gray-700 rounded-t-lg">
+        <div className="flex items-center justify-between ps-3">
+          <input
+            id="global"
+            type="checkbox"
+            className="w-4 h-4 accent-purple-600 bg-gray-100 border-gray-300 dark:border-gray-700 rounded focus:ring-blue-500 dark:focus:ring-blue-800"
+            checked={globalCheck}
+            style={{ width: 14 }}
+            onChange={(e) => handleGlobalChange(e.target.checked)}
+          />
+          <label
+            htmlFor="global"
+            className="w-full py-3 ms-2 text-sm font-medium text-gray-90"
+          >
+            {globalCheck ? t("hideAll") : t("showAll")}
+          </label>
+        </div>
+      </div>
+      <menu className="w-full h-full overflow-y-auto scrollbar">
+        {[...layers.entries()].map((e) => (
+          <li
+            key={e[0]}
+            className="w-full border-b border-gray-200 dark:border-gray-700 rounded-t-lg"
+          >
+            <div className="flex items-center ps-3">
+              <input
+                id={e[0]}
+                type="checkbox"
+                className="w-4 h-4 accent-purple-600 bg-gray-100 border-gray-300 dark:border-gray-700 rounded focus:ring-blue-500 dark:focus:ring-blue-800"
+                checked={e[1].visible}
+                onChange={() => handleOnChange(e[0], e[1])}
+              ></input>
+              <label
+                htmlFor={e[0]}
+                className="w-full py-3 ms-2 text-sm font-medium text-gray-90"
+              >
+                {e[1].name}
+              </label>
+            </div>
+          </li>
+        ))}
+      </menu>
+    </div>
   );
 }
