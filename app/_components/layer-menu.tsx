a1d8f58fcd971b55e4b3ad6419f251e87f87384b
courtneypattison <courtneyp@ttison.ca>
2024-04-13 07:57:04 -0400

    Fix #171

--- app/_components/layer-menu.tsx
+++ app/_components/layer-menu.tsx
@@ -1,9 +1,8 @@
 import { Layer } from "@/_lib/interfaces/layer";
 import { Dispatch, SetStateAction, useMemo } from "react";
 import { IconButton } from "@/_components/buttons/icon-button";
-import CloseIcon from "@/_icons/close-icon";
 import { useTranslations } from "next-intl";
-import LayersIcon from "@/_icons/layers-icon";
+import KeyboardArrowLeftIcon from "@/_icons/keyboard-arrow-left";
 
 export default function LayerMenu({
   className,
@@ -41,49 +40,36 @@ export default function LayerMenu({
 
   return (
     <div
-      className={`${className} ${visible ? "left-0" : "-left-60"} w-48 h-[calc(100vh-9rem)] text-sm font-medium text-gray-900 dark:text-white bg-white dark:bg-black border border-gray-200 dark:border-gray-700 border-top-0 absolute z-50 transition-all duration-700`}
+      className={`${className} ${visible ? "left-0" : "-left-60"} w-48 ${layers.size > 0 && layers.size * 40 < 200 ? "h-fit" : "h-[calc(100vh-9rem)]"}  text-sm font-medium text-gray-900 dark:text-white bg-white dark:bg-black border border-gray-200 dark:border-gray-700 border-top-0 absolute z-50 transition-all duration-700`}
     >
       <h1
         key="global"
-        className="w-full border-b border-gray-200 dark:border-gray-700 rounded-t-lg"
+        className="w-fullrounded-t-lg border-b border-gray-200 dark:border-gray-700"
       >
-        <div className="flex items-center space-between ml-2">
-          <LayersIcon ariaLabel={t("title")} />
-          <h6 className="ml-2">{t("title")}</h6>
-          <IconButton onClick={() => setVisible(false)} className="ml-auto">
-            <CloseIcon ariaLabel="close" />
+        <div className="flex items-center space-between">
+          <IconButton onClick={() => setVisible(false)}>
+            <KeyboardArrowLeftIcon ariaLabel="close" />
           </IconButton>
-        </div>
-      </h1>
-      <div className="w-full border-b border-gray-200 dark:border-gray-700 rounded-t-lg">
-        <div className="flex items-center justify-between ps-3">
-          <input
-            id="global"
-            type="checkbox"
-            className="w-4 h-4 accent-purple-600 bg-gray-100 border-gray-300 dark:border-gray-700 rounded focus:ring-blue-500 dark:focus:ring-blue-800"
-            checked={globalCheck}
-            style={{ width: 14 }}
-            onChange={(e) => handleGlobalChange(e.target.checked)}
-          />
-          <label
-            htmlFor="global"
-            className="w-full py-3 ms-2 text-sm font-medium text-gray-90"
+          <h6 className="ml-2">{t("title")}</h6>
+          <button
+            className="text-purple-600 ml-auto px-4 hover:text-purple-800 "
+            onClick={() => handleGlobalChange(!globalCheck)}
           >
             {globalCheck ? t("hideAll") : t("showAll")}
-          </label>
+          </button>
         </div>
-      </div>
-      <menu className="w-full h-full overflow-y-auto scrollbar">
+      </h1>
+
+      <menu
+        className={`w-full ${layers.size > 0 && layers.size * 40 > 200 ? "h-[calc(100vh-12rem)] overflow-y-auto scrollbar" : ""}`}
+      >
         {[...layers.entries()].map((e) => (
-          <li
-            key={e[0]}
-            className="w-full border-b border-gray-200 dark:border-gray-700 rounded-t-lg"
-          >
+          <li key={e[0]} className="w-full  rounded-t-lg">
             <div className="flex items-center ps-3">
               <input
                 id={e[0]}
                 type="checkbox"
-                className="w-4 h-4 accent-purple-600 bg-gray-100 border-gray-300 dark:border-gray-700 rounded focus:ring-blue-500 dark:focus:ring-blue-800"
+                className="w-4 h-4 accent-purple-600  rounded focus:ring-blue-500 dark:focus:ring-blue-800"
                 checked={e[1].visible}
                 onChange={() => handleOnChange(e[0], e[1])}
               ></input>
