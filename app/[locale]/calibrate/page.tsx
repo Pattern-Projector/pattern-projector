7e1a3c215717aceb85282f110b2b1721002f234d
courtneypattison <courtneyp@ttison.ca>
2024-04-16 15:53:59 -0400

    Change calibrate page width and height to be based on screen

--- app/[locale]/calibrate/page.tsx
+++ app/[locale]/calibrate/page.tsx
@@ -307,12 +307,12 @@ export default function Page() {
     <main
       onPointerDown={handlePointerDown}
       ref={noZoomRefCallback}
-      className={`${isDarkTheme(displaySettings.theme) && "dark bg-black"} w-full h-full absolute overflow-hidden touch-none`}
+      className={`${isDarkTheme(displaySettings.theme) && "dark bg-black"} w-screen h-screen absolute overflow-hidden touch-none`}
     >
-      <div className="bg-white dark:bg-black dark:text-white w-full h-full">
+      <div className="bg-white dark:bg-black dark:text-white w-screen h-screen">
         <FullScreen
           handle={fullScreenHandle}
-          className="bg-white dark:bg-black transition-all duration-500 w-full h-full"
+          className="bg-white dark:bg-black transition-all duration-500 w-screen h-screen"
         >
           {showCalibrationAlert ? (
             <h2 className="flex items-center gap-4 absolute left-1/4 top-1/2 w-1/2 bg-white dark:bg-black dark:text-white z-[150] p-4 rounded">
