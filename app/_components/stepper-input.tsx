13c947c492a0537b6777671f5921b6b68a8942c2
courtneypattison <courtneyp@ttison.ca>
2024-05-20 15:42:12 -0400

    Change stepper input style to work on tablets

--- app/_components/stepper-input.tsx
+++ app/_components/stepper-input.tsx
@@ -58,48 +58,46 @@ export default function StepperInput({
     }
   }
 
+  const buttonClassName =
+    "dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 p-2.5 h-11 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-800 focus:outline-none user-select-none";
+  const inputClass =
+    "rounded-none h-11 p-2.5 bg-gray-50 border border-gray-300 text-gray-900 text-sm focus:ring-4 focus:outline-none focus:ring-blue-300 w-12 text-center dark:bg-black dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-800 dark:focus:border-blue-800";
+
   return (
-    <div className={`flex items-center ${className}`}>
+    <div className="flex items-center justify-center">
       <label
-        className="text-sm font-medium text-gray-500 dark:text-white mr-1"
-        htmlFor={id}
+        style={{ WebkitUserSelect: "none", userSelect: "none" }}
+        className="text-sm font-medium text-gray-900 dark:text-white mr-1 user-select-none"
+        htmlFor={name}
       >
         {label}
       </label>
-      <div className="relative items-center z-0">
-        <button
-          type="button"
-          name={name}
-          className="absolute top-0 left-1 h-full flex items-center"
-          onPointerDown={() => handleStep(-1)}
-          onPointerUp={handleButtonRelease}
-          onPointerLeave={handleButtonRelease}
-        >
-          <StepDownIcon ariaLabel="test" />
-        </button>
-        <input
-          min={min}
-          type={type ? type : "text"}
-          className={`${inputClassName} bg-gray-50 text-center p-2.5 border border-gray-300 dark:border-gray-600 text-gray-900 text-sm rounded-lg focus:ring-4 focus:outline-none dark:bg-black dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-800 dark:focus:border-blue-800`}
-          id={id}
-          data-test-id={inputTestId}
-          name={name}
-          onChange={(e: ChangeEvent<HTMLInputElement>) => handleChange(e)}
-          required
-          value={value}
-          ref={inputRef}
-        />
-        <button
-          type="button"
-          name={name}
-          className="absolute top-0 right-1 h-full flex items-center"
-          onPointerDown={() => handleStep(1)}
-          onPointerUp={handleButtonRelease}
-          onPointerLeave={handleButtonRelease}
-        >
-          <StepUpIcon ariaLabel="test" />
-        </button>
-      </div>
+      <button
+        style={{ WebkitUserSelect: "none", userSelect: "none" }}
+        className={`${buttonClassName} rounded-s-lg`}
+        onPointerDown={() => handleStep(-1)}
+        onPointerUp={handleButtonRelease}
+        onPointerLeave={handleButtonRelease}
+      >
+        <StepDownIcon ariaLabel="decrement" />
+      </button>
+      <input
+        onChange={handleChange}
+        inputMode="numeric"
+        value={value}
+        name={name}
+        id={name}
+        className={`${inputClassName} ${inputClass}`}
+      />
+      <button
+        style={{ WebkitUserSelect: "none", userSelect: "none" }}
+        className={`${buttonClassName} rounded-e-lg`}
+        onPointerDown={() => handleStep(1)}
+        onPointerUp={handleButtonRelease}
+        onPointerLeave={handleButtonRelease}
+      >
+        <StepUpIcon ariaLabel="increment" />
+      </button>
     </div>
   );
 }
--- app/_icons/step-down-icon.tsx
+++ app/_icons/step-down-icon.tsx
@@ -1,14 +1,14 @@
 export default function StepDownIcon({ ariaLabel }: { ariaLabel: string }) {
   return (
     <svg
-      aria-label={ariaLabel}
       xmlns="http://www.w3.org/2000/svg"
-      height="24"
+      height="24px"
       viewBox="0 -960 960 960"
-      width="24"
+      width="24px"
       fill="currentColor"
+      aria-label={ariaLabel}
     >
-      <path d="M280-440h400v-80H280v80ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z" />
+      <path d="M200-440v-80h560v80H200Z" />
     </svg>
   );
 }
