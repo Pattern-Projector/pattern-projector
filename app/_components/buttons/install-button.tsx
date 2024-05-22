06758e51875d8760435bafea274a361f349eae75
courtneypattison <courtneyp@ttison.ca>
2024-05-21 21:54:33 -0400

    Fix build warnings and update CHANGELOG

--- app/_components/buttons/install-button.tsx
+++ app/_components/buttons/install-button.tsx
@@ -112,7 +112,7 @@ export default function InstallButton() {
           <ModalActions>
             <button
               className="text-purple-700 hover:text-white border border-purple-700 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-purple-400 dark:text-purple-400 dark:hover:text-white dark:hover:bg-purple-500 dark:focus:ring-purple-900"
-              onClick={(e) => {
+              onClick={() => {
                 setShowModal(false);
               }}
             >
