4230e9421dea3b6709460f45a396ea03f937f526
Charlotte Curtis <ccurtis@mtroyal.ca>
2024-05-22 21:38:52 -0600

    Kind of working, but I suspect pdf-lib is buggy

--- app/_lib/pdfstitcher.ts
+++ app/_lib/pdfstitcher.ts
@@ -1,4 +1,9 @@
-import { PDFDocument } from "@cantoo/pdf-lib";
+import {
+  PDFDocument,
+  PDFName,
+  PDFObjectCopier,
+  PDFObject,
+} from "@cantoo/pdf-lib";
 import { StitchSettings } from "@/_lib/interfaces/stitch-settings";
 import { getPageNumbers } from "./get-page-numbers";
 
@@ -21,6 +26,23 @@ function trimmedPageSize(
   return { width: width, height: height };
 }
 
+async function initNewDoc(inDoc: PDFDocument) {
+  /**
+   * Creates a new document, copying over metadata and various other
+   * properties (especially layer info)
+   */
+  const outDoc = await PDFDocument.create();
+  const copier = PDFObjectCopier.for(inDoc.context, outDoc.context);
+  const pagesKey = PDFName.of("Pages");
+  for (const [key, value] of inDoc.catalog.entries()) {
+    if (key != pagesKey) {
+      outDoc.catalog.set(key, copier.copy(value));
+    }
+  }
+
+  return outDoc;
+}
+
 export async function saveStitchedPDF(
   file: File,
   settings: StitchSettings,
@@ -30,10 +52,12 @@ export async function saveStitchedPDF(
   // Grab the bytes from the file object and try to load the PDF
   // Error handling is done in the calling function.
   const pdfBytes = await file.arrayBuffer();
-  const doc = await PDFDocument.load(pdfBytes, {
+  const inDoc = await PDFDocument.load(pdfBytes, {
     ignoreEncryption: true,
     password,
   });
+
+  const outDoc = await initNewDoc(inDoc);
 
   // Trust that the user is happy with the layout
   const pages = getPageNumbers(settings.pageRange, pageCount);
@@ -42,14 +66,14 @@ export async function saveStitchedPDF(
   const trim = settings.edgeInsets;
 
   // Compute the size of the output document
-  const pageSize = trimmedPageSize(doc, pages, settings);
+  const pageSize = trimmedPageSize(inDoc, pages, settings);
   const outWidth = pageSize.width * cols;
   const outHeight = pageSize.height * rows;
 
   // Create a new page to hold the stitched pages
   // Add at least a 1" margin because of weirdness.
   const margin = Math.max(trim.horizontal, trim.vertical, 72);
-  const outPage = doc.addPage([outWidth, outHeight]);
+  const outPage = outDoc.addPage();
   outPage.setMediaBox(
     -margin,
     -margin,
@@ -59,25 +83,21 @@ export async function saveStitchedPDF(
 
   // Loop through the pages and copy them to the output document
   let x = 0;
-  let y = outHeight - pageSize.height - margin;
+  let y = outHeight - pageSize.height;
+
   for (const p of pages) {
     if (p > 0) {
-      const xobject = await doc.embedPage(doc.getPage(p - 1));
+      const xobject = await outDoc.embedPage(inDoc.getPage(p - 1));
       outPage.drawPage(xobject, { x: x, y: y });
     }
     // Adjust the position for the next page
     x += pageSize.width;
-    if (x >= outWidth) {
+    if (x > outWidth - margin) {
       x = 0;
       y -= pageSize.height;
     }
   }
 
-  // remove all the original pages except the last one
-  while (doc.getPageCount() > 1) {
-    doc.removePage(0);
-  }
-
   // Save the stitched document
-  return await doc.save();
+  return await outDoc.save();
 }
