7f6c29720c87ccf8b6892aa1834c5dc4d13a52c4
courtneypattison <courtneyp@ttison.ca>
2024-09-10 11:14:20 -0400

    Make layer menu background opaque #321

--- app/_components/layer-menu.tsx
+++ app/_components/layer-menu.tsx
@@ -31,7 +31,7 @@ export default function LayerMenu({
   return (
     <>
       <div
-        className={`${className ?? ""} pointer-events-auto absolute z-20 mt-2 ${visible ? "left-0" : "-left-60"} w-48 ${hasLayers && numberOfLayers * 40 < 200 ? "h-fit" : "h-[calc(100vh-12rem)]"}  text-sm font-medium text-gray-900 dark:text-white bg-opacity-60 dark:bg-opacity-50 bg-white dark:bg-black border border-gray-200 dark:border-gray-700 border-top-0 absolute transition-all duration-700`}
+        className={`${className ?? ""} pointer-events-auto absolute z-20 mt-2 ${visible ? "left-0" : "-left-60"} w-48 ${hasLayers && numberOfLayers * 40 < 200 ? "h-fit" : "h-[calc(100vh-12rem)]"}  text-sm font-medium text-gray-900 dark:text-white bg-white dark:bg-black border border-gray-200 dark:border-gray-700 border-top-0 absolute transition-all duration-700`}
       >
         <h1
           key="global"
