bc7eb297f618839f63bca0a10df2cb573ab192e7
courtneypattison <courtneyp@ttison.ca>
2024-09-24 09:54:21 -0400

    Fix button hover state

--- app/_components/theme/styles.ts
+++ app/_components/theme/styles.ts
@@ -5,6 +5,6 @@ export enum ButtonStyle {
 
 export function getButtonStyleClasses(style: ButtonStyle) {
   return style === ButtonStyle.OUTLINE
-    ? `flex gap-2 items-center hover:text-white border border-2 border-solid focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center`
+    ? `flex gap-2 items-center hover:text-white dark:hover:text-white border border-2 border-solid focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center`
     : `flex gap-2 items-center text-white focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 focus:outline-none`;
 }
