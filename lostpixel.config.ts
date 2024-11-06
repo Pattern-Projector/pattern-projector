b3ad9c2ef07f7b4a1d65915d63b2266cb8c80792
courtneypattison <courtneyp@ttison.ca>
2024-11-06 10:24:58 -0500

    Add calibrate and landing page to pageShots

--- lostpixel.config.ts
+++ lostpixel.config.ts
@@ -3,11 +3,8 @@ import { CustomProjectConfig } from "lost-pixel";
 export const config: CustomProjectConfig = {
   pageShots: {
     pages: [
-      { path: "/app", name: "app" },
-      { path: "/next-app", name: "next-app" },
-      { path: "/next-app?name=App", name: "next-app-with-query-param" },
-      { path: "/fetch", name: "fetch-static-props" },
-      { path: "/client-fetch", name: "fetch-client" },
+      { path: "/", name: "app" },
+      { path: "/calibrate", name: "calibrate" },
     ],
     baseUrl: "http://172.17.0.1:3000",
   },
