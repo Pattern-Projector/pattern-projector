1641aa1361ea70f655da7769340f9adf0c509ff0
courtneypattison <courtneyp@ttison.ca>
2025-03-02 09:42:05 -0500

    Fix merge conflict

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -578,8 +578,7 @@ export default function Header({
               )}
               {t("openPDF")}
             </label>
-            <button
-              className="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900"
+            <Button
               onClick={handleCalibrateProjectButtonClick}
               className="flex align-middle"
               style={ButtonStyle.FILLED}
