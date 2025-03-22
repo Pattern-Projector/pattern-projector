740cfc474e0eb2d68f173f7e81b9b80f19163c22
courtneypattison <courtneyp@ttison.ca>
2025-03-22 06:39:45 -0400

    Fix broken full screen

--- app/_lib/full-screen.ts
+++ app/_lib/full-screen.ts
@@ -3,7 +3,11 @@ import { FullScreenHandle } from "react-full-screen";
 // Type error in react-full-screen so need to wrap in a try-catch
 export function toggleFullScreen(fullScreenHandle: FullScreenHandle): void {
   try {
-    fullScreenHandle.active ? fullScreenHandle.exit : fullScreenHandle.enter;
+    if (fullScreenHandle.active) {
+      fullScreenHandle.exit();
+    } else {
+      fullScreenHandle.enter();
+    }
   } catch (error) {
     console.error("Error toggling full screen", error);
   }
